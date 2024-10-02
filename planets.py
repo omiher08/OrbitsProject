2a128118e62957c30db5cab9fe3f22c101d0792d
omiher08 <omaruniverse06@gmail.com>
2024-10-02 11:05:41 -0500

    Update planets.py
    Added a color label to the earth class.
    Created CelestialObject class, to develop all the calculations for the objects. Because before, we were doing the calculations in the class itself, but later we are going to repeated when adding more objects, in order to do not repeat the code, we created a class apart that's able to calculate this for us

--- planets.py
+++ planets.py
@@ -1,20 +1,26 @@
 from decimal import Decimal
 import numpy as np
 
-class Earth():
+class CelestialObject():
+    def __init__(self, aphelion, perihelion):
+        self.semimajor_axis = Decimal((aphelion + perihelion)/2)
+        self.focal_distance = Decimal(aphelion - self.semimajor_axis)
+        self.semiminor_axis = Decimal(np.sqrt(self.semimajor_axis**2 - self.focal_distance**2))
+        self.eccentricity = Decimal(self.focal_distance/self.semimajor_axis)
+        self.referenceRadius = Decimal(self.semiminor_axis**2 / self.semimajor_axis)
+
+class Earth(CelestialObject):
     def __init__(self):
         self.name = "Earth"
+        self.color = "green"
         self.aphelion = Decimal("1.017")
         self.perihelion = Decimal("0.983")
-        self.semimajor_axis = Decimal((self.aphelion + self.perihelion)/2)
-        self.focal_distance = Decimal(self.aphelion - self.semimajor_axis)
-        self.semiminor_axis = round(Decimal(np.sqrt(self.semimajor_axis**2 - self.focal_distance**2)), 10)
-        self.eccentricity = Decimal(self.focal_distance/self.semimajor_axis)
-        self.referenceRadius = round(Decimal(self.semiminor_axis**2 / self.semimajor_axis), 10)
+        super().__init__(self.aphelion, self.perihelion)
 
 def main():
     planet = Earth()
     print("Planet: " + planet.name)
+    print(f"Color: {planet.color}")
     print(f"Aphelion: {planet.aphelion} au")
     print(f"Perihelion: {planet.perihelion} au")
     print(f"Semimajor - axis: {planet.semimajor_axis} au")
