10b462162cd81406aefdd6ca280546eb82d4f3fb
omiher08 <omaruniverse06@gmail.com>
2024-10-03 17:51:44 -0500

    Update orbits.py
    Added graph in polar coordinates and optimized it

--- orbits.py
+++ orbits.py
@@ -1,4 +1,4 @@
-import planets
+from objects import *
 import numpy as np
 import matplotlib.pyplot as plt
 import random
@@ -11,25 +11,31 @@ def cartesian_coordinates(planet, ax):
     ax.plot(x, -y, color="#2f7bf5")
     ax.plot(-planet.focal_distance, 0, marker="o", markersize=8, label="Sun", color="#dbc70d")
     point = random.choice(range(0,n_points))
+    x_point, y_point = x[point], y[point]
     y_axis_of_earth = random.choice(["+", "-"])
-    if y_axis_of_earth == "+":
-        ax.plot(x[point], y[point], marker="o", markersize=8, label=f"{planet.name}", color="green")
-    else:
-        ax.plot(x[point], -y[point], marker="o", markersize=8, label=f"{planet.name}", color="green")
+    if y_axis_of_earth == "-":
+        y_point = -y_point
+    ax.plot(x_point, y_point, marker="o", markersize=8, label=f"{planet.name}", color=f"{planet.color}")
     ax.axis("equal")
-    ax.set_title(f"{planet.name}'s Orbit in cartesian coordinates", fontsize=8)
+    ax.set_title(f"{planet.name}'s Orbit in cartesian coordinates", fontsize=10)
     ax.set_xlabel("X (au)")
     ax.set_ylabel("Y (au)")
     ax.grid()
-    ax.legend()
 
 def polar_coordinates(planet, ax):
     n_points = 10000
-    ax.set_title(f"{planet.name}'s Orbit in polar coordinates", fontsize=8)
+    theta = np.linspace(0, 2*np.pi, n_points)
+    r = float(planet.referenceRadius) / (1 + float(planet.eccentricity) * np.cos(theta))
+    ax.plot(theta, r, color="#2f7bf5")
+    ax.plot(0, 0, marker="o", markersize=8, color="#dbc70d")
+    point = random.choice(range(0,n_points))
+    theta_point, r_point = theta[point], r[point]
+    ax.plot(theta_point, r_point, marker="o", markersize=8, color=f"{planet.color}")
+    ax.set_title(f"{planet.name}'s Orbit in polar coordinates", fontsize=10)    
     ax.grid(color='gray', linestyle='--', linewidth=0.5)
 
 def main():
-    planet = planets.Earth()
+    planet = Earth()
     fig = plt.figure()
     s1 = fig.add_subplot(2, 2, 1)
     s2 = fig.add_subplot(2, 2, 2, polar=True)
@@ -37,6 +43,7 @@ def main():
     s4 = fig.add_subplot(2, 2, 4)
     cartesian_coordinates(planet, s1)
     polar_coordinates(planet, s2)
+    fig.legend()
     plt.tight_layout()
     plt.show()
 
