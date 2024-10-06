054d03d67b2930099c19dc6484d98b92a1ad10d6
omarher01 <omaruniverse01@gmail.com>
2024-10-06 18:51:41 -0500

    Update orbits.py
    Adjust central bodies charateristics to vary

--- orbits.py
+++ orbits.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-def cartesian_coordinates(orbitingobject, ax):
+def cartesian_coordinates(orbitingobject, centralbody, ax):
     n_points = 10000
     x = np.linspace(-orbitingobject.semimajor_axis, orbitingobject.semimajor_axis, n_points)
     y = orbitingobject.semiminor_axis * np.sqrt(1-(x**2/orbitingobject.semimajor_axis**2))
     ax.plot(x, y, color="#2f7bf5")
     ax.plot(x, -y, color="#2f7bf5")
-    ax.plot(-orbitingobject.focal_distance, 0, marker="o", markersize=8, label="Sun", color="#dbc70d")
+    ax.plot(-orbitingobject.focal_distance, 0, marker="o", markersize=8, label=centralbody.name, color=centralbody.color)
     point = random.choice(range(0,n_points))
     x_point, y_point = x[point], y[point]
     y_axis_of_earth = random.choice(["+", "-"])
@@ -22,12 +22,12 @@ def cartesian_coordinates(orbitingobject, ax):
     ax.set_ylabel("Y (au)")
     ax.grid()
 
-def polar_coordinates(orbitingobject, ax):
+def polar_coordinates(orbitingobject, centralbody, ax):
     n_points = 10000
     theta = np.linspace(0, 2*np.pi, n_points)
     r = float(orbitingobject.referenceRadius) / (1 + float(orbitingobject.eccentricity) * np.cos(theta))
     ax.plot(theta, r, color="#2f7bf5")
-    ax.plot(0, 0, marker="o", markersize=8, color="#dbc70d")
+    ax.plot(0, 0, marker="o", markersize=8, color=centralbody.color)
     point = random.choice(range(0,n_points))
     theta_point, r_point = theta[point], r[point]
     ax.plot(theta_point, r_point, marker="o", markersize=8, color=f"{orbitingobject.color}")
@@ -35,15 +35,16 @@ def polar_coordinates(orbitingobject, ax):
     ax.grid(color='gray', linestyle='--', linewidth=0.5)
 
 def main():
-    orbitingobject = Earth()
+    orbitingobject = Mercury()
+    centralbody = Sun()
     orbitingobject.printOrbitingValues()
     fig = plt.figure()
     s1 = fig.add_subplot(2, 2, 1)
     s2 = fig.add_subplot(2, 2, 2, polar=True)
     s3 = fig.add_subplot(2, 2, 3)
     s4 = fig.add_subplot(2, 2, 4)
-    cartesian_coordinates(orbitingobject, s1)
-    polar_coordinates(orbitingobject, s2)
+    cartesian_coordinates(orbitingobject, centralbody, s1)
+    polar_coordinates(orbitingobject, centralbody, s2)
     fig.legend()
     plt.tight_layout()
     plt.show()
