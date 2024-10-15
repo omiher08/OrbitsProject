50fc347a4a1f7f5be4cc4c0cea6f109bb383065b
omiher08 <omaruniverse06@gmail.com>
2024-10-15 18:35:19 -0500

    Update orbits.py
    Allow manual selection of planets and customization.
    Added a function to ensure aphelion and perihelion are numbers

--- orbits.py
+++ orbits.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 import random
 from objects import *
 
+def is_decimal(number):
+    try:
+        float(number)
+        return True
+    except ValueError:
+        return False
+
 def cartesian_coordinates(orbitingBody, centralBody, ax):
     n_points = 10000
     x = np.linspace(-orbitingBody.semimajor_axis, orbitingBody.semimajor_axis, n_points)
@@ -34,8 +41,61 @@ def polar_coordinates(orbitingBody, centralBody, ax):
     ax.grid(color='gray', linestyle='--', linewidth=0.5)
 
 def main():
-    orbitingBody = EARTH
-    centralBody = SUN
+    while True:
+        try:
+            ask = int(input("""Which object would you like to graph?
+                            
+1. Mercury
+                            
+2. Venus
+                            
+3. Earth
+                            
+4. Mars
+                            
+5. Jupiter
+                            
+6. Saturn
+                            
+7. Uranus
+                            
+8. Neptune
+                            
+9. Custom
+                            
+Choose (1-9): """).strip())
+            if 0 < ask < 10:
+                if ask == 9:
+                    object_name = input("\nEnter object's name: ").strip().capitalize()
+                    while True:
+                        object_aphelion = input("Enter object's aphelion in au (Use '.' for decimals): ").strip().capitalize()
+                        ok = is_decimal(object_aphelion)
+                        if ok:
+                            break
+                        else:
+                            print("\nNot a valid aphelion")
+                    while True:
+                        object_perihelion = input("Enter object's perihelion in au (Use '.' for decimals): ").strip().capitalize()
+                        ok = is_decimal(object_perihelion)
+                        if ok:
+                            break
+                        else:
+                            print("\nNot a valid perihelion")
+                    orbitingBody = OrbitingBody(object_name, "black", object_aphelion, object_perihelion)
+                    centralBody_name = input("\nEnter central body's name (body that your object orbits) (Write 'Sun' if the central body is the sun): ").strip().capitalize()
+                    if centralBody_name == "Sun":
+                        centralBody = SUN
+                    else:
+                        centralBody = CentralBody(centralBody_name, "#918c8c")
+                else:
+                    orbitingBody = PLANETS[ask-1]
+                    centralBody = SUN
+                print()
+                break
+            else:
+                print("\nYou must enter a number between 1 and 9.")
+        except ValueError:
+            print("\nThat's not a valid input.")
     orbitingBody.printOrbitingValues()
     fig = plt.figure()
     s1 = fig.add_subplot(2, 2, 1)
